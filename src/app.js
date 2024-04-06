8d669e4fca0fa8621b4356d8dca83822a51c77e3
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-04-06 02:30:17 +0000

    almost done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let cards = ["heart", "club", "diamond", "spade"];
+let colors = ["red", "black"];
+
+function getRandomInt() {
+  let randomNumber = Math.random() * 10;
+  return Math.floor(randomNumber) + 2;
+}
+
+function getRandomColor(color) {
+  let randomColor = Math.floor(Math.random() * color.length);
+}
+
+function getRandomCard(cards) {
+  let randomCardIndex = Math.floor(Math.random() * cards.length);
+  let choice = cards[randomCardIndex];
+  console.log(choice);
+
+  /*switch (choice) {
+    case "heart":
+      document.querySelectorAll("#heart").forEach(function(element) {
+        element.style.color = "red";
+      });
+      break;
+    default:
+      break;
+  }*/
+}
+
+getRandomCard(cards);
+
+const buttonExcuse = document.getElementById("excuseButton");
+
+buttonExcuse.addEventListener("click", function() {
+  location.reload();
+});
+
+window.onload = () => {
+  document.getElementById("number").innerHTML = getRandomInt();
 };
