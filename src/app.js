dbf356106cfaeacc9b7935a2df6d4417dcab6fa0
Georges Abdel Junior Joseph <99396455+deldevlop@users.noreply.github.com>
2024-04-08 18:17:34 +0000

    Card Generator Done

--- src/app.js
+++ src/app.js
@@ -1,39 +1,96 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 let cards = ["heart", "club", "diamond", "spade"];
 let colors = ["red", "black"];
 
 function getRandomInt() {
-  let randomNumber = Math.random() * 10;
-  return Math.floor(randomNumber) + 2;
+  let randomNumber = Math.floor(Math.random() * 14) + 1;
+  switch (randomNumber) {
+    case 1:
+      return "A";
+      break;
+    case 11:
+      return "J";
+      break;
+    case 12:
+      return "Q";
+      break;
+    case 13:
+      return "K";
+      break;
+    case 14:
+      return "A";
+      break;
+    default:
+      return randomNumber;
+  }
 }
 
-function getRandomColor(color) {
-  let randomColor = Math.floor(Math.random() * color.length);
+function getRandomColor(colors) {
+  let randomColorIndex = Math.floor(Math.random() * colors.length);
+  let colorChoice = colors[randomColorIndex];
+  let numberElement = document.getElementById("number");
+  switch (colorChoice) {
+    case "red":
+      console.log(colorChoice);
+      document.querySelectorAll("#elementCard").forEach(function(element) {
+        element.style.color = "red";
+      });
+      numberElement.style.color = "red";
+      break;
+    case "black":
+      console.log(colorChoice);
+      document.querySelectorAll("#elementCard").forEach(function(element) {
+        element.style.color = "black";
+      });
+      numberElement.style.color = "black";
+      break;
+    default:
+      document.querySelectorAll("#elementCard").forEach(function(element) {
+        element.style.color = "black";
+      });
+      numberElement.style.color = "black";
+  }
 }
 
 function getRandomCard(cards) {
   let randomCardIndex = Math.floor(Math.random() * cards.length);
   let choice = cards[randomCardIndex];
-  console.log(choice);
 
-  /*switch (choice) {
+  switch (choice) {
     case "heart":
-      document.querySelectorAll("#heart").forEach(function(element) {
-        element.style.color = "red";
+      console.log(choice);
+      document.querySelectorAll("#elementCard").forEach(function(element) {
+        element.className = "bi bi-suit-heart-fill";
+        getRandomColor(colors);
+      });
+      break;
+    case "club":
+      console.log(choice);
+      document.querySelectorAll("#elementCard").forEach(function(element) {
+        element.className = "bi bi-suit-club-fill";
+        getRandomColor(colors);
+      });
+      break;
+    case "diamond":
+      console.log(choice);
+      document.querySelectorAll("#elementCard").forEach(function(element) {
+        element.className = "bi bi-suit-diamond-fill";
+        getRandomColor(colors);
+      });
+      break;
+    case "spade":
+      console.log(choice);
+      document.querySelectorAll("#elementCard").forEach(function(element) {
+        element.className = "bi bi-suit-spade-fill";
+        getRandomColor(colors);
       });
       break;
     default:
       break;
-  }*/
+  }
 }
-
-getRandomCard(cards);
 
 const buttonExcuse = document.getElementById("excuseButton");
 
@@ -43,4 +100,5 @@ buttonExcuse.addEventListener("click", function() {
 
 window.onload = () => {
   document.getElementById("number").innerHTML = getRandomInt();
+  getRandomCard(cards);
 };
